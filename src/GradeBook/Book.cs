5c8604d327b642c7e9b389a5e70e0258f9465ffd
Janek <janekokularczyk@gmail.com>
2022-04-22 20:32:22 +0200

    updating project with xunit testing

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
-    class Book 
+    public class Book 
     {
         public Book(string name) 
         {
@@ -15,21 +15,21 @@ namespace GradeBook
             grades.Add(grade);
         }
 
-        public void ShowStatistics() 
+        public Statistics GetStatistics() 
         {
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
             foreach (double grade in grades) 
             {
-                highGrade = Math.Max(grade, highGrade);
-                lowGrade = Math.Min(grade, lowGrade);
-                result += grade;
+                result.High = Math.Max(grade, result.High);
+                result.Low = Math.Min(grade, result.Low);
+                result.Average += grade;
             }
-            result /= grades.Count;
-            Console.WriteLine($"The lowest grade is {lowGrade}");
-            Console.WriteLine($"The highest grade is {highGrade}");
-            Console.WriteLine($"The average grade is {result:N1}");
+            result.Average /= grades.Count;
+
+            return result;
         }
 
         private List<double> grades;
