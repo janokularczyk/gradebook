8eb78738a78bafd30ae78381a5fcf9cde183f77f
Janek <janekokularczyk@gmail.com>
2022-04-25 15:21:01 +0200

    updating project

--- src/GradeBook/InMemoryBook.cs
+++ src/GradeBook/InMemoryBook.cs
@@ -56,39 +56,10 @@ namespace GradeBook
         public override Statistics GetStatistics()
         {
             var result = new Statistics();
-            result.Average = 0.0;
-            result.High = double.MinValue;
-            result.Low = double.MaxValue;
 
             foreach(double grade in grades) 
             {
-                result.High = Math.Max(grade, result.High);
-                result.Low = Math.Min(grade, result.Low);
-                result.Average += grade;
-            }
-            result.Average /= grades.Count;
-
-            switch(result.Average) 
-            {
-                case var d when d >= 90.0:
-                    result.Letter = 'A';
-                    break;
-
-                case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-
-                case var d when d >= 70.0:
-                    result.Letter = 'C';
-                    break;
-
-                case var d when d >= 60.0:
-                    result.Letter = 'D';
-                    break;
-
-                default:
-                    result.Letter = 'F';
-                    break;
+                result.Add(grade);
             }
 
             return result;
